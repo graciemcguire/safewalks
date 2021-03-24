0c85e8eb9b85617d35c27bf707c9dccb49cda28e
Gracie <m.graciemcguire@gmail.com>
2021-03-24 19:42:11 -0400

    updates podfile, adds icons to search bar

--- src/components/HomeSearch/index.js
+++ src/components/HomeSearch/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
+import styles from './styles';
+
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+AntDesign.loadFont();
+MaterialIcons.loadFont();
+
 const HomeSearch = () => {
   return (
     <View>
-      <Text>hello hi</Text>
       {/* { input box} */}
-      <View style={{styles.input}}></View>
-        <Text style={{styles.inputText}}>where to?</Text>
-        <View style={{styles.timeContainer}}>
-            
+      <View style={styles.inputBox}>
+        <Text style={styles.inputText}>Where To?</Text>
+
+        <View style={styles.timeContainer}>
+          <AntDesign name={'clockcircle'} size={16} color={'#535353'} />
+          <Text>Now</Text>
+          <MaterialIcons name={'keyboard-arrow-down'} size={16} />
         </View>
-    {/* { previous destination} */}
-    {/* { home} */}
+        {/* { previous destination} */}
+        {/* { home} */}
+      </View>
     </View>
   );
 };
---
+++ src/components/HomeSearch/styles.js
@@ -0,0 +1,32 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  inputBox: {
+    backgroundColor: '#e7e7e7',
+    margin: 10,
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  inputText: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#535353',
+  },
+  timeContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 100,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderRadius: 100,
+  },
+  //   learnMore: {
+  //     color: 'white',
+  //     fontSize: 15,
+  //     fontWeight: 'bold',
+  //   }
+});
+
+export default styles;
