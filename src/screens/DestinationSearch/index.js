76dab80ecf3efeef95a62b27d2e5bd085fdce540
Gracie <m.graciemcguire@gmail.com>
2021-03-25 20:55:07 -0400

    finish destination search set up

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -5,8 +5,8 @@ import styles from './styles.js';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
 
 const DestinationSearch = () => {
-  // const API_KEY = `${process.env.REACT_APP_GOOGLE_API_KEY}`;
-  const API_KEY = 'AIzaSyCuB25qpctYqZhJaz2Hj27xXqVvUpeYhSM';
+  const API_KEY = `${process.env.REACT_APP_GOOGLE_API_KEY}`;
+
 
   const [originPlace, setOriginPlace] = useState({initialState: null});
   const [destinationPlace, setDestinationPlace] = useState({
@@ -18,7 +18,7 @@ const DestinationSearch = () => {
       console.warn('redirect to results page');
     }
   }, [originPlace, destinationPlace]);
-  
+  //
   return (
     <SafeAreaView>
       <View style={styles.container}>
