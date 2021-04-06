71dfe9a1563f4326dca730b71c8e05b1717bd69d
Gracie <m.graciemcguire@gmail.com>
2021-04-06 14:56:26 -0400

    adds types component and stylesheet

--- src/screens/SearchResults/index.js
+++ src/screens/SearchResults/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {View, Text} from 'react-native';
 
 import HomeMap from '../../components/HomeMap'
+import Types from '../../components/Types'
 
 const SearchResults = () => {
   return (
     <View>
       <HomeMap />
+      <Types />
     </View>
   );
 };
